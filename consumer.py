3cb355823c3cc4aa83764dab8f8d23c35abbce8d
Mattias Persson <mattiasbpersson@gmail.com>
2021-10-08 21:09:36 +0200

    raspberry pi play sound support

--- consumer.py
+++ consumer.py
@@ -1,30 +1,45 @@
 import wave
 
 import pyaudio
-from playsound import playsound
 
 from Client import Client, QualityOfService
 from EventHandler import EventHandler
 from Wav import Wav
 
 def playWav(client, event: Wav):
-    RATE = 44100
     CHUNK = 512
+    RATE = 44100
+    with open('test.wav', 'w+b') as f:
 
+        wav_fp = wave.open(f, 'wb')
+        wav_fp.setnchannels(1)
+        wav_fp.setsampwidth(pyaudio.get_sample_size(pyaudio.paInt16))
+        wav_fp.setframerate(RATE*1.3)
+        wav_fp.writeframes(b''.join(event.wav_blob))
+        wav_fp.close()
+    
+    wf = wave.open('test.wav', 'rb')
     p = pyaudio.PyAudio()
 
-    stream = p.open(format=pyaudio.paInt16,
-                    channels=1,
-                    rate=RATE,
-                    input_device_index=3,
+    # open stream (2)
+    stream = p.open(format=p.get_format_from_width(wf.getsampwidth()),
+                    channels=wf.getnchannels(),
+                    rate=wf.getframerate(),
                     output=True)
 
-    stream.write(b''.join(event.wav_blob))
-    #data = wf.readframes(CHUNK)
+    # read data
+    data = wf.readframes(CHUNK)
 
+    # play stream (3)
+    while len(data) > 0:
+        stream.write(data)
+        data = wf.readframes(CHUNK)
+
+    # stop stream (4)
     stream.stop_stream()
     stream.close()
 
+    # close PyAudio (5)
     p.terminate()
 
 if __name__ == '__main__':
